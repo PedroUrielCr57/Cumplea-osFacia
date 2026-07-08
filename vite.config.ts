5b0342b1158f555862da6a70ebcd3d5827e09d34
PedroUrielCr57 <pedrourielcruzrivera@gmail.com>
2026-07-07 22:57:26 -0600

    Agregar deploy para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/CumpleFacia/',
+  base: '/Cumplea-osFacia/',
   plugins: [react(), tailwindcss()],
 })
